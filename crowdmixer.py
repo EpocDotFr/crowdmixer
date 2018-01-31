05b013686ee41ce4b7f6a54ca98ea11b04ac519d
EpocDotFr <contact@epoc.fr>
2018-01-31 08:48:35 +0100

    Put hooks in their own file

--- crowdmixer.py
+++ crowdmixer.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, make_response, g, request
 from logging.handlers import RotatingFileHandler
-from werkzeug.exceptions import HTTPException
 from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
 from flask_babel import Babel
+from flask import Flask
 import logging
 import arrow
 
@@ -58,47 +57,4 @@ app.jinja_env.globals.update(arrow=arrow)
 import routes
 import models
 import commands
-
-
-# -----------------------------------------------------------
-# HTTP errors handler
-
-
-@app.errorhandler(401)
-@app.errorhandler(403)
-@app.errorhandler(404)
-@app.errorhandler(500)
-@app.errorhandler(503)
-def http_error_handler(error, without_code=False):
-    if isinstance(error, HTTPException):
-        error = error.code
-    elif not isinstance(error, int):
-        error = 500
-
-    body = render_template('errors/{}.html'.format(error))
-
-    if not without_code:
-        return make_response(body, error)
-    else:
-        return make_response(body)
-
-
-# -----------------------------------------------------------
-# Hooks
-
-
-@app.before_request
-def set_locale():
-    if not hasattr(g, 'CURRENT_LOCALE'):
-        if app.config['FORCE_LANGUAGE']:
-            g.CURRENT_LOCALE = app.config['FORCE_LANGUAGE']
-        else:
-            g.CURRENT_LOCALE = request.accept_languages.best_match(app.config['LANGUAGES'].keys(), default=app.config['DEFAULT_LANGUAGE'])
-
-
-@babel.localeselector
-def get_app_locale():
-    if not hasattr(g, 'CURRENT_LOCALE'):
-        return app.config['DEFAULT_LANGUAGE']
-    else:
-        return g.CURRENT_LOCALE
+import hooks
---
+++ hooks.py
@@ -0,0 +1,39 @@
+from flask import render_template, make_response, g, request
+from werkzeug.exceptions import HTTPException
+from crowdmixer import app, babel
+
+
+@app.before_request
+def set_locale():
+    if not hasattr(g, 'CURRENT_LOCALE'):
+        if app.config['FORCE_LANGUAGE']:
+            g.CURRENT_LOCALE = app.config['FORCE_LANGUAGE']
+        else:
+            g.CURRENT_LOCALE = request.accept_languages.best_match(app.config['LANGUAGES'].keys(), default=app.config['DEFAULT_LANGUAGE'])
+
+
+@babel.localeselector
+def get_app_locale():
+    if not hasattr(g, 'CURRENT_LOCALE'):
+        return app.config['DEFAULT_LANGUAGE']
+    else:
+        return g.CURRENT_LOCALE
+
+
+@app.errorhandler(401)
+@app.errorhandler(403)
+@app.errorhandler(404)
+@app.errorhandler(500)
+@app.errorhandler(503)
+def http_error_handler(error, without_code=False):
+    if isinstance(error, HTTPException):
+        error = error.code
+    elif not isinstance(error, int):
+        error = 500
+
+    body = render_template('errors/{}.html'.format(error))
+
+    if not without_code:
+        return make_response(body, error)
+    else:
+        return make_response(body)
